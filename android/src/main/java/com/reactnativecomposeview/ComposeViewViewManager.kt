6688954826114f1cc9315110f9b8ab2b60ff0f73
Denis Razumeev <razumeev@scand.com>
2021-10-27 15:24:17 +0300

    feat: more sweet compose ui

--- android/src/main/java/com/reactnativecomposeview/ComposeViewViewManager.kt
+++ android/src/main/java/com/reactnativecomposeview/ComposeViewViewManager.kt
@@ -7,10 +7,20 @@ import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class ComposeViewViewManager : SimpleViewManager<View>() {
   override fun getName() = "ComposeViewView"
@@ -34,14 +44,37 @@ class ComposeViewViewManager : SimpleViewManager<View>() {
         setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
         setContent {
             MaterialTheme {
-                // In Compose world
-                Text("Hello Applicaster!This is Compose view added in RN environment")
-
-                Text("Hello 2s")
+                MessageCard("Hello Applicaster!", "This is Compose view added in RN environment")
             }
         }
     }
     }
+
+    @Preview
+    @Composable
+    fun MessageCard(title: String = "default title", message: String = "default message") {
+        Column {
+            Image(
+                painter = painterResource(R.drawable.applicaster_logo),
+                contentDescription = "Contact profile picture",
+            )
+
+            Column {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.h5,
+                    modifier = Modifier.padding(all = 4.dp)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = message,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(all = 6.dp)
+                )
+            }
+        }
+    }
+
 
 }
 fun startComposeActivity(context: Context?) {
