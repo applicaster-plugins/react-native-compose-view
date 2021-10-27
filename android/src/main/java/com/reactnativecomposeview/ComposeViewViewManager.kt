f957da010579abda3a3f80bd3a36fe38dbded6e0
Denis Razumeev <razumeev@scand.com>
2021-10-27 14:03:31 +0300

    feat: basic compose activity

--- android/src/main/java/com/reactnativecomposeview/ComposeViewViewManager.kt
+++ android/src/main/java/com/reactnativecomposeview/ComposeViewViewManager.kt
@@ -1,10 +1,12 @@
 package com.reactnativecomposeview
 
+import android.content.Context
 import android.graphics.Color
 import android.view.View
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
+import android.content.Intent
 
 class ComposeViewViewManager : SimpleViewManager<View>() {
   override fun getName() = "ComposeViewView"
@@ -16,5 +18,12 @@ class ComposeViewViewManager : SimpleViewManager<View>() {
   @ReactProp(name = "color")
   fun setColor(view: View, color: String) {
     view.setBackgroundColor(Color.parseColor(color))
+      view.setOnClickListener {
+          startComposeActivity(view.context)
+      }
   }
+}
+fun startComposeActivity(context: Context?) {
+    val intent = Intent(context, ComposeActivity::class.java)
+    context?.startActivity(intent)
 }
